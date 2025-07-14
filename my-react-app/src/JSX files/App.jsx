c952b97297db36391758233897a85b2fc88c60b0
shashankanand1409 <shashankanand1409@gmail.com>
2025-07-14 12:17:07 +0530

    creating basic file structure

---
+++ my-react-app/src/JSX files/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import '../CSS files/App.css'
+import Component from './Component'
+
+
+  return (
+    <>
+      
+    </>
+  )
+
+
+export default App
